93ca141b7de7ee4e3a93f0b98a6f0650ef9d7cfc
Paresh-95 <Pareshbhamare95@gmail.com>
2024-09-12 23:44:58 +0530

    Added city name

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Akatsuki Coding Club</h1>
       <h2>The RCPIT</h2>
+      <h2>Shirpur</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
