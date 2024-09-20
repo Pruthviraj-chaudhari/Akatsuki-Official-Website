d8a96fb9b54a2e1a59327e194feb82f299d558f3
Bhumika Patil <bhumikapatil2204@gmail.com>
2024-09-20 14:36:40 +0530

    final commit

--- src/Data/eventImages .ts
+++ src/Data/eventImages .ts
@@ -1,13 +1,118 @@
 export const eventImages = [
-    { name: 'Internal SIH 2024', photos: ['../images/Alumni/shivang.jpg', '../images/Alumni/durgesh.jpg', '../images/Alumni/shivang3.jpg'] },
-    { name: 'ML Workshop', photos: ['../images/Alumni/ravi1.jpg', '../images/Alumni/ravi2.jpg'] },
-    { name: 'Converges 2024', photos: ['../images/Alumni/vinay1.jpg', '../images/Alumni/vinay2.jpg', '../images/Alumni/vinay3.jpg'] },
-    { name: 'HCL founder meet', photos: ['../images/Alumni/shreya1.jpg', '../images/Alumni/shreya2.jpg'] },
-    { name: 'Nodevember S1', photos: ['../images/Alumni/shubham1.jpg', '../images/Alumni/shubham2.jpg', '../images/Alumni/shubham3.jpg'] },
-    { name: 'Converges 2023', photos: ['../images/ditective/detect-1_scrm36.jpg', '../images/ditective/detect-2_ihbs0f.jpg','../images/ditective/detect-3_sqlo6p.jpg','../images/ditective/detect-4_xlkru2.jpg','../images/ditective/detect-5_ohuf4o.jpg','../images/ditective/detect-6_ysvmkg.jpg','../images/ditective/detect-7_py3wsh.jpg','../images/ditective/detect-8_suisan.jpg','../images/ditective/detect-9_teiruk.jpg','../images/ditective/detect-10_h9vgdl.jpg','../images/ditective/detect-11_mhy9g6.png'] },
-    { name: 'Internal SIH 2023', photos: ['../images/SIH-2023/20230923_15303PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_22732PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_24408PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_25939PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_32101PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_115459AMByGPSMapCamera.jpg'] },
-    { name: 'Tech hunt 2022', photos: [ '../images/tech-hunt/teamAkatsuki6_cmfaas.jpg','../images/tech-hunt/tech-1_ve9ht0.jpg','../images/tech-hunt/tech-7_vkour8.jpg','../images/tech-hunt/tech-3_jtl0hw.jpg','../images/tech-hunt/tech-4_gr5z7u.jpg','../images/tech-hunt/tech-5_balpn0.jpg','../images/tech-hunt/tech-6_v7mmmm.jpg','../images/tech-hunt/tech-2_ignqmd.jpg'] },
-    { name: 'Hacky Holi 2022', photos: ['../images/hackyholi/WhatsApp Image 2024-09-20 at 11.58.38 AM (1).jpeg,'] },
-    { name: 'Workshop', photos: ['../images/portfolio/portfolio-1_ilepmt.jpg','../images/portfolio/portfolio-2_kfk0vy.jpg','../images/portfolio/portfolio-3_xpucdc.jpg','../images/portfolio/portfolio-4_upeumx.jpg','../images/portfolio/portfolio-5_cj0lzg.jpg','../images/portfolio/portfolio-6_gfdczb.jpg','../images/portfolio/portfolio-7_lsqpif.jpg','../images/portfolio/portfolio-8_ar1yx1.jpg','../images/portfolio/portfolio-9_ci3kq6.jpg','../images/portfolio/portfolio-10_x3f8eq.jpg','../images/portfolio/portfolio-11_rzu74s.jpg'] },
-     // Add more events and photos as needed
-  ];
+  {
+    name: "Converges 2024",
+    photos: [
+      "../images/Converges2024/WhatsApp Image 2024-09-20 at 1.52.31 PM.jpeg",
+      "../images/Converges2024/WhatsApp Image 2024-09-20 at 1.53.34 PM.jpeg",
+      "../images/Converges2024/WhatsApp Image 2024-09-20 at 1.55.05 PM.jpeg",
+    ],
+  },
+  {
+    name: "Internal SIH 2024",
+    photos: [
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.05 PM (1).jpeg",
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.06 PM.jpeg",
+      "images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.07 PM (1).jpeg",
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.06 PM.jpeg",
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.08 PM.jpeg",
+    ],
+  },
+  // {
+  //   name: "ML Workshop",
+  //  photos: ["../images/Alumni/ravi1.jpg", "../images/Alumni/ravi2.jpg"],
+  // },
+
+  {
+    name: "HCL founder meet",
+    photos: [
+      "./images/hcl_Event/WhatsApp Image 2024-09-20 at 1.20.59 PM.jpeg",
+      "../images/hcl_Event/WhatsApp Image 2024-09-20 at 1.21.03 PM.jpeg",
+    ],
+  },
+  {
+    name: "Nodevember S1",
+    photos: [
+      "../images/nodemember/WhatsApp Image 2024-09-20 at 1.20.53 PM (1).jpeg",
+      "../images/nodemember/WhatsApp Image 2024-09-20 at 1.20.53 PM.jpeg",
+      "../images/nodemember/WhatsApp Image 2024-09-20 at 1.20.54 PM (2).jpeg",
+      "../images/nodemember/WhatsApp Image 2024-09-20 at 1.20.55 PM (1).jpeg",
+      "../images/nodemember/WhatsApp Image 2024-09-20 at 1.20.57 PM.jpeg",
+      "../images/nodemember/WhatsApp Image 2024-09-20 at 1.21.05 PM (1).jpeg",
+    ],
+  },
+  {
+    name: "Converges 2023",
+    photos: [
+      "../images/ditective/detect-1_scrm36.jpg",
+      "../images/ditective/detect-2_ihbs0f.jpg",
+      "../images/ditective/detect-3_sqlo6p.jpg",
+      "../images/ditective/detect-4_xlkru2.jpg",
+      "../images/ditective/detect-5_ohuf4o.jpg",
+      "../images/ditective/detect-6_ysvmkg.jpg",
+      "../images/ditective/detect-7_py3wsh.jpg",
+      "../images/ditective/detect-8_suisan.jpg",
+      "../images/ditective/detect-9_teiruk.jpg",
+      "../images/ditective/detect-10_h9vgdl.jpg",
+      "../images/ditective/detect-11_mhy9g6.png",
+    ],
+  },
+  {
+    name: "Internal SIH 2023",
+    photos: [
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.05 PM (1).jpeg",
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.05 PM.jpeg",
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.06 PM (1).jpeg",
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.06 PM.jpeg",
+      "../images/SIH_2024/WhatsApp Image 2024-09-20 at 1.47.08 PM.jpeg",
+    ],
+  },
+  {
+    name: "Tech hunt 2022",
+    photos: [
+      "../images/tech-hunt/teamAkatsuki6_cmfaas.jpg",
+      "../images/tech-hunt/tech-1_ve9ht0.jpg",
+      "../images/tech-hunt/tech-2_ignqmd.jpg",
+      "../images/tech-hunt/tech-3_jtl0hw.jpg",
+      "../images/tech-hunt/tech-4_gr5z7u.jpg",
+      "../images/tech-hunt/tech-5_balpn0.jpg",
+      "../images/tech-hunt/tech-6_v7mmmm.jpg",
+      "../images/tech-hunt/tech-7_vkour8.jpg",
+    ],
+  },
+  /* {
+    name: "Hacky Holi 2022",
+    photos: [
+      "",
+    ],
+  },*/
+  {
+    name: "Workshop",
+    photos: [
+      "../images/portfolio/portfolio-1_ilepmt.jpg",
+      "../images/portfolio/portfolio-2_kfk0vy.jpg",
+      "../images/portfolio/portfolio-3_xpucdc.jpg",
+      "../images/portfolio/portfolio-4_upeumx.jpg",
+      "../images/portfolio/portfolio-5_cj0lzg.jpg",
+      "../images/portfolio/portfolio-6_gfdczb.jpg",
+      "../images/portfolio/portfolio-7_lsqpif.jpg",
+      "../images/portfolio/portfolio-8_ar1yx1.jpg",
+      "../images/portfolio/portfolio-9_ci3kq6.jpg",
+      "../images/portfolio/portfolio-10_x3f8eq.jpg",
+      "../images/portfolio/portfolio-11_rzu74s.jpg",
+    ],
+  },
+  // Add more events and photos as needed
+];
+
+//  */   { name: 'Internal SIH 2024', photos: ['../images/Alumni/shivang.jpg', '../images/Alumni/durgesh.jpg', '../images/Alumni/shivang3.jpg'] },
+//     { name: 'ML Workshop', photos: ['../images/Alumni/ravi1.jpg', '../images/Alumni/ravi2.jpg'] },
+//     { name: 'Converges 2024', photos: ['../images/Alumni/vinay1.jpg', '../images/Alumni/vinay2.jpg', '../images/Alumni/vinay3.jpg'] },
+//     { name: 'HCL founder meet', photos: ['../images/Alumni/shreya1.jpg', '../images/Alumni/shreya2.jpg'] },
+//     { name: 'Nodevember S1', photos: ['../images/Alumni/shubham1.jpg', '../images/Alumni/shubham2.jpg', '../images/Alumni/shubham3.jpg'] },
+//     { name: 'Converges 2023', photos: ['../images/ditective/detect-1_scrm36.jpg', '../images/ditective/detect-2_ihbs0f.jpg','../images/ditective/detect-3_sqlo6p.jpg','../images/ditective/detect-4_xlkru2.jpg','../images/ditective/detect-5_ohuf4o.jpg','../images/ditective/detect-6_ysvmkg.jpg','../images/ditective/detect-7_py3wsh.jpg','../images/ditective/detect-8_suisan.jpg','../images/ditective/detect-9_teiruk.jpg','../images/ditective/detect-10_h9vgdl.jpg','../images/ditective/detect-11_mhy9g6.png'] },
+//     { name: 'Internal SIH 2023', photos: ['../images/SIH-2023/20230923_15303PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_22732PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_24408PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_25939PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_32101PMByGPSMapCamera.jpg','../images/SIH-2023/20230923_115459AMByGPSMapCamera.jpg'] },
+//     { name: 'Tech hunt 2022', photos: [ '../images/tech-hunt/teamAkatsuki6_cmfaas.jpg','../images/tech-hunt/tech-1_ve9ht0.jpg','../images/tech-hunt/tech-7_vkour8.jpg','../images/tech-hunt/tech-3_jtl0hw.jpg','../images/tech-hunt/tech-4_gr5z7u.jpg','../images/tech-hunt/tech-5_balpn0.jpg','../images/tech-hunt/tech-6_v7mmmm.jpg','../images/tech-hunt/tech-2_ignqmd.jpg'] },
+//     { name: 'Hacky Holi 2022', photos: ['../images/hackyholi/WhatsApp Image 2024-09-20 at 11.58.38 AM (1).jpeg,'] },
+//     { name: 'Workshop', photos: ['../images/portfolio/portfolio-1_ilepmt.jpg','../images/portfolio/portfolio-2_kfk0vy.jpg','../images/portfolio/portfolio-3_xpucdc.jpg','../images/portfolio/portfolio-4_upeumx.jpg','../images/portfolio/portfolio-5_cj0lzg.jpg','../images/portfolio/portfolio-6_gfdczb.jpg','../images/portfolio/portfolio-7_lsqpif.jpg','../images/portfolio/portfolio-8_ar1yx1.jpg','../images/portfolio/portfolio-9_ci3kq6.jpg','../images/portfolio/portfolio-10_x3f8eq.jpg','../images/portfolio/portfolio-11_rzu74s.jpg'] },/
+// Add more events and photos as needed
+//   ];
