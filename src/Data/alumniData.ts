e7e0ac6af47a8d26f4bd35e2716c5a7c07785f11
Bhupesh Patil <160297608+BhupeshPatil5130@users.noreply.github.com>
2024-09-27 00:51:14 +0530

    added data in 2024 alumini

--- src/Data/alumniData.ts
+++ src/Data/alumniData.ts
@@ -90,29 +90,105 @@ export const alumni2023: AlumniMember[] = [
 //2024 batch
 export const alumni2024: AlumniMember[] = [
   {
-    name: 'Mangesh Bide',
-    role: 'Ex-President',
-    company: ['Netwin'],
-    image: '../images/Alumni/mangeshbide.jpg',
-    batch: 2024
+    name: "Mangesh Bide",
+    role: "Ex-President",
+    company: ["Netwin-4.5LPA"],
+    image: "../images/Alumni/mangeshbide.jpg",
+    batch: 2024,
   },
   {
-    name: 'Sakshi Kukreja',
-    role: 'Ex-Vice President',
-    company: ['Netwin'],
-    image: '../images/Alumni/sakshi.jpg',
-    batch: 2024
+    name: "Sakshi Kukreja",
+    role: "Ex-Vice President",
+    company: ["Tech Mahindra - 3.25 LPA"],
+    image: "../images/Alumni/sakshi.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Ravindra Pawar",
+    role: "MERN Stack Developer",
+    company: ["Zitics-4 LPA "],
+    image: "../images/Alumni/ravi.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Durgesh Mahajan",
+    role: "Fullstack Developer",
+    company: ["RecuiteCRM- 7 LPA "],
+    image: "../images/Alumni/durgesh.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Vinay Salunkhe",
+    role: "Backend Developer",
+    company: ["TSS- 4.2 to 4.8 LPA "],
+    image: "../images/Alumni/vinay.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Harsh Gawali",
+    role: "Fullstack Developer",
+    company: [". "],
+    image: "../images/Alumni/harsh.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Suchita Patil",
+    role: "MERN Stack Developer",
+    company: ["QualityKiosk -2.5 To 5.5 LPA "],
+    image: "../images/Alumni/suchita.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Samihan Nandedkar",
+    role: "Backend Developer",
+    company: [". "],
+    image: "../images/Alumni/samihan.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Rutuja Chavan",
+    role: "Backend Developer",
+    company: ["Persistent-5.01 LPA","TSS-4.2 to 4.8 LPA"],
+    image: "../images/Alumni/rutuja.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Nikita Jangid",
+    role: "Frontend Developer",
+    company: ["."],
+    image: "../images/Alumni/nikita.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Prajakta Patil",
+    role: "Frontend Developer",
+    company: ["QualityKiosk - 2.5 To 5.5 LPA","Persistent-5.01 LPA"],
+    image: "../images/Alumni/prajakta.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Prajakta Patil",
+    role: "Frontend Developer",
+    company: ["QualityKiosk - 2.5 To 5.5 LPA","Persistent-5.01 LPA"],
+    image: "../images/Alumni/prajakta.jpg",
+    batch: 2024,
+  },
+  {
+    name: "Gitanjali Patil",
+    role: "Frontend Developer",
+     company: ["QualityKiosk - 2.5 To 5.5 LPA","Persistent-5.01 LPA"],
+    image: '../images/Alumni/gitanjali.jpeg',
+    batch: 2024,
   },
 ];
-  
 
 //2025 batch
 export const alumni2025: AlumniMember[] = [
   {
-    name: 'Pruthviraj Chaudhari',
-      role: 'President',
-      image: '../images/members/Pruthviraj.png', 
-      company: ['TSS'],
-      batch: 2025
+    name: "Pruthviraj Chaudhari",
+    role: "President",
+    image: "../images/members/Pruthviraj.png",
+    company: ["TSS"],
+    batch: 2025,
   },
 ];
