134a2d41124b0aa1fed94f0e122351f904ddf3df
Bhupesh Patil <160297608+BhupeshPatil5130@users.noreply.github.com>
2024-09-26 23:38:02 +0530

    Updated Vision & Mission component

--- src/Data/missionData.ts
+++ src/Data/missionData.ts
@@ -1,33 +1,36 @@
 // Define the Mission interface
 interface Mission {
-    iconClass: string;
-    title: string;
-    description: string;
+  iconClass: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+const missionData: Mission[] = [
+  {
+    iconClass: 'bx bx-coding',
+    title: '1.',
+    description: 'To build a coding culture and instill the habits needed for success in product-based companies.',
+    imageUrl: './altumcode-mCj7UinqOYQ-unsplash.jpg',
+  },
+  {
+    iconClass: 'bx bx-lightbulb',
+    title: '2.',
+    description: 'To ignite innovation and equip students with tools for transformation and a bright future.',
+    imageUrl: './altumcode-P2SkP_PXhlU-unsplash.jpg',
+  },
+  {
+    iconClass: 'bx bx-brain',
+    title: '3.',
+    description: ' To foster curiosity and experimentation in a collaborative, transdisciplinary learning environment',
+    imageUrl: './altumcode-PNbDkQ2DDgM-unsplash.jpg',
+  },
+  {
+    iconClass: 'bx bx-heart',
+    title: '4.',
+    description: 'To inspire confidence and curiosity by eliminating coding phobia and fostering a passion for coding.',
+    imageUrl: './oskar-yildiz-cOkpTiJMGzA-unsplash.jpg',
   }
-  
-  // Create the mission data array with typed data
-  const missionData: Mission[] = [
-    {
-      iconClass: 'bx bx-coding', // Replace with actual icon class
-      title: '1.',
-      description: 'Our mission is to establish a coding culture in our students and inculcate the coding habits necessary to crack product-based companies.',
-    },
-    {
-      iconClass: 'bx bx-lightbulb', // Replace with actual icon class
-      title: '2.',
-      description: 'To spark a flame of innovation and positive change in the students, giving them all the necessary tools to transform their lives and achieve a bright future.',
-    },
-    {
-      iconClass: 'bx bx-brain', // Replace with actual icon class
-      title: '3.',
-      description: 'To cultivate curiosity, encourage experimentation, and prepare students for success in an interconnected world through a transdisciplinary, non-hierarchical learning environment.',
-    },
-    {
-      iconClass: 'bx bx-heart', // Replace with actual icon class
-      title: '4.',
-      description: 'To inspire confidence and curiosity by eliminating coding phobia from students, fostering a passion for coding and problem-solving.',
-    }
-  ];
-  
-  export default missionData;
-  
+];
+
+export default missionData;
